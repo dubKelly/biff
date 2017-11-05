c2f78a6b7ce16604c2a5cad0f7fb9c5f5afa187d
dubKelly <js.neeb1780@gmail.com>
2017-11-05 13:40:59 -0500

    landing page transitions (pre nav menu)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,20 @@
-var sections = document.getElementsByTagName('section');
-var sectionIndex = 0;
-var menuBtn = document.getElementsByClassName('menuBtn');
+(function nav() {
+	var sections = document.getElementsByTagName('section');
+	var sectionIndex = 0;
+	var menuBtn = document.getElementsByClassName('menuBtn');
 
-for (var i = 0; i < menuBtn.length; i++) {
-	menuBtn[i].onclick = function() {
-		var children = sections[sectionIndex].childNodes;
-		
-		for (var i = children.length - 1; i >= 0; i--) {
-			if (children[i].classList == undefined) {
-				continue;
-			}
-			if (children[i].classList.contains('toggle')) {
-				children[i].classList.toggle('open');
+	for (var i = 0; i < menuBtn.length; i++) {
+		menuBtn[i].onclick = function() {
+			var children = sections[sectionIndex].childNodes;
+			
+			for (var i = children.length - 1; i >= 0; i--) {
+				if (children[i].classList == undefined) {
+					continue;
+				}
+				if (children[i].classList.contains('toggle')) {
+					children[i].classList.toggle('open');
+				}
 			}
 		}
 	}
-}
+})();
